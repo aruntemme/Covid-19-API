1ce18152046d388e0e05ecb7f7abe3b9cafb76a8
aruntemme <arungauthamk@gmail.com>
2020-03-23 10:33:38 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, request, jsonify
+import json
+import os
+
+from refresh import update
+from apscheduler.schedulers.background import BackgroundScheduler
+
+app = Flask(__name__)
+
+def sensor():
+    update()
+    print("Scheduler is alive!")
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(sensor,'interval',minutes=10)
+sched.start()
+
+
+@app.route('/')
+def index():
+    with open('data.json') as f:
+        d = json.load(f)
+    return d
+
+@app.route('/confirmed')
+def confirmed():
+    with open('data.json') as f:
+        d = json.load(f)
+    return d['confirmed']
+
+@app.route('/deaths')
+def deaths():
+    with open('data.json') as f:
+        d = json.load(f)
+    return d['deaths']
+
+@app.route('/recovered')
+def recovered():
+    with open('data.json') as f:
+        d = json.load(f)
+    return d['recovered']
+
+@app.route('/latest')
+def latest():
+    with open('data.json') as f:
+        d = json.load(f)
+    return d['latest']
+
+@app.route('/country')
+def getCountryData():
+    countryCode = request.args.get('countryCode')
+    print("----",countryCode)
+    with open('dataCountry.json') as f:
+        d = json.load(f)
+    if(countryCode in d):
+        return d[countryCode]
+    elif(countryCode == None):
+        return '{}'
+    else:
+        return d
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
